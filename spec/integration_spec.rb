7b802bbf1ffba1671ae9f8b41d1897394bf1e672
Cody Brubaker <cody.brubaker@gmail.com>
2016-12-09 15:58:45 -0800

    add second passing integration spec to add stylist

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -10,4 +10,13 @@ describe('see stylist list', {:type => :feature}) do
     expect(page).to have_content("Your Current Stylist")
   end
 
+describe('add stylist', {:type => :feature}) do
+  it('allows user to add stylist') do
+    visit('/stylists')
+    fill_in('name', :with => 'Cody')
+    click_button('Add')
+    expect(page).to have_content("Here are your stylists")
+  end
+end
+
 end
