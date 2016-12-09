a8a03d65fca6493b055f535723ef5ffd7a999264
Cody <cody.brubaker@gmail.com>
2016-12-08 20:33:51 -0800

    add save and find methods

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -8,11 +8,37 @@ describe(Stylist) do
     end
   end
 
-  describe('#all') do
+  describe('.all') do
     it('empty array at first') do
       expect(Stylist.all()).to(eq([]))
     end
   end
+
+  describe('#==') do
+    it('is the same stylist if it has the same name') do
+      stylist1 = Stylist.new({:id => nil, :name => 'Cody'})
+      stylist2 = Stylist.new({:id => nil, :name => 'Cody'})
+      expect(stylist1).to(eq(stylist2))
+    end
+  end
+
+  describe('.save') do
+    it('saves user input to database') do
+      test_stylist = Stylist.new({:id => nil, :name => 'Cody'})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe(".find") do
+   it("returns a stylist by its ID") do
+     test_stylist1 = Stylist.new({:id => nil, :name => 'Cody'})
+     test_stylist1.save()
+     test_stylist2 = Stylist.new({:id => nil, :name => 'John'})
+     test_stylist2.save()
+     expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+   end
+ end
 
 
 
